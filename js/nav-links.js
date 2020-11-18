1d3ac2ed364378680a03c3602cf9f3b83d106448
iriquintero <iriangel.quintero@gmail.com>
2020-11-18 16:45:05 -0300

    crear el toggler nav

---
+++ js/nav-links.js
@@ -0,0 +1,21 @@
+const navSlider = ()=>{
+    const burguer= document.querySelector('.burguer');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burguer.addEventListener('click',()=>{
+        //toggle nav
+        nav.classList.toggle('nav-active');
+        //animate links
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation){
+                link.style.animation= '';
+            } else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+            }
+        });
+        burguer.classList.toggle('toggle');
+    });
+
+}
+navSlider();
